53a1f47d2b7be78269898188c6c70c6ec05c2595
zhuang002 <zhuang001@gmail.com>
2023-01-15 10:12:57 -0500

    correct version. Legacy algorithm

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,8 @@ public class Main {
 			trees.add(new Position(sc.nextInt()-1, sc.nextInt()-1));
 		}
 		
-		
-		for (int squareLen = n; squareLen>=0; squareLen--) {
+		// O(n^3*t)
+		for (int squareLen = n; squareLen>=0; squareLen--) { // loop n times
 			if (trySquare(squareLen)) {
 				System.out.println(squareLen);
 				return;
@@ -23,9 +23,9 @@ public class Main {
 		}
 	}
 	
-	private static boolean trySquare(int squareLen) {
+	private static boolean trySquare(int squareLen) { // O(n^2*t)
 		// TODO Auto-generated method stub
-		for (int x=0;x<n;x++) {
+		for (int x=0;x<n;x++) { // loop n*n times
 			for (int y=0;y<n;y++) {
 				Position pos = new Position(x,y);
 				if (tryPosLen(pos, squareLen)) {
@@ -37,9 +37,9 @@ public class Main {
 		
 	}
 
-	private static boolean tryPosLen(Position pos, int squareLen) {
+	private static boolean tryPosLen(Position pos, int squareLen) { // O(t)
 		// TODO Auto-generated method stub
-		if (pos.x+squareLen>=n || pos.y+squareLen>=n) {
+		if (pos.x+squareLen>n || pos.y+squareLen>n) {
 			return false;
 		}
 		
@@ -52,8 +52,8 @@ public class Main {
 
 	private static boolean containsTree(Position pos, int squareLen) {
 		// TODO Auto-generated method stub
-		for (Position treePos: trees) {
-			if (treePos.x>=pos.x && treePos.x<=pos.x+squareLen && treePos.y>=pos.y && treePos.y<=pos.y+squareLen)
+		for (Position treePos: trees) { // loop t times
+			if (treePos.x>=pos.x && treePos.x<pos.x+squareLen && treePos.y>=pos.y && treePos.y<pos.y+squareLen)
 				return true;
 		}
 		return false;
